133a84faa3f482797a9e315f1fdc1ac7e3d49d26
hinami7002 <125355289+hinami7002@users.noreply.github.com>
2023-03-09 09:48:39 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 G B F C D A E ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("C D E F G A B C5 ", 120)
+})
